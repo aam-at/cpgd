14fbb8e272bd19eae9b31d9167cdb8dd5f0e3b0b
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-04-21 12:36:06 +0800

    Update data cifar10 load

--- cifar10/data.py
+++ cifar10/data.py
@@ -1,9 +1,10 @@
 import numpy as np
-import tensorflow as tf
 import tensorflow.keras.datasets.cifar10 as cifar10
 
 
-def load_cifar10(validation_size=10000, train_indices=False, shuffle=True,
+def load_cifar10(validation_size=10000,
+                 shuffle=True,
+                 data_format="NHWC",
                  seed=123):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -11,6 +12,15 @@ def load_cifar10(validation_size=10000, train_indices=False, shuffle=True,
     x_test = np.cast[np.float32](x_test)
     y_train = np.reshape(np.cast[np.int64](y_train), (-1, ))
     y_test = np.reshape(np.cast[np.int64](y_test), (-1, ))
+
+    def x_transform(x):
+        if data_format == "NHWC":
+            return x
+        elif data_format == "NCHW":
+            return np.transpose(x, (0, 3, 1, 2))
+        else:
+            raise ValueError
+
     # split train/validation
     total_train_examples = x_train.shape[0]
     indices = np.arange(total_train_examples)
@@ -20,11 +30,5 @@ def load_cifar10(validation_size=10000, train_indices=False, shuffle=True,
     train_examples = total_train_examples - validation_size
     x_val, y_val = x_train[train_examples:], y_train[train_examples:]
     x_train, y_train = x_train[:train_examples], y_train[:train_examples]
-    if train_indices:
-        train_ds = tf.data.Dataset.from_tensor_slices(
-            (x_train, y_train, np.arange(x_train.shape[0], dtype=np.int64)))
-    else:
-        train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-    return train_ds, val_ds, test_ds
+    return ((x_transform(x_train), y_train), (x_transform(x_val), y_val),
+            (x_transform(x_test), y_test))
