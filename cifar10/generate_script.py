dc1d3ddaef517696ffd22177bb0f3a4e58c990a7
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-04-27 18:00:57 +0800

    Update generate scripts

--- cifar10/generate_script.py
+++ cifar10/generate_script.py
@@ -25,23 +25,23 @@ def generate_test_optimizer_lp(norm, load_from, **kwargs):
 
 def test_lp_config(norm, runs=1, master_seed=1):
     assert norm in ['l0', 'li', 'l1', 'l2']
-    num_batches = {'l0': 10, 'li': 10, 'l1': 10, 'l2': 5}[norm]
-    num_batches = 1
-    attack_args = {'norm': norm, 'num_batches': num_batches}
-    name = "cifar10_lin_"
+    num_images = {'l0': 1000, 'li': 1000, 'l1': 1000, 'l2': 500}[norm]
+    batch_size = 500
+    attack_args = {'norm': norm, 'num_batches': num_images // 5, 'batch_size': batch_size}
+    name = "cifar10_lin_lambd_"
     for (model, iterations, max_iterations, loss, optimizer, accelerated,
          gradient_normalize,
          use_sign,
          adaptive_momentum,
          dual_ema) in itertools.product(models, [500], [1], ["cw"],
-                                        ["sgd"], [False, True],
+                                        ["sgd"], [False],
                                         [False, True],
                                         [False, True],
                                         [True, False],
                                         [True]):
         max_iterations *= iterations
         type = Path(model).stem.split("_")[-1]
-        working_dir = f"../results/cifar10_hs/test_{norm}_{type}"
+        working_dir = f"../results/cifar10_10/test_{norm}_{type}"
         name0 = name + f"{norm}_{type}_{iterations}_{max_iterations // 1000}k_"
         name0 = name0 + f"{optimizer if not accelerated else optimizer + '_apg'}_{loss}_"
         name0 = name0 + f"{'gnorm' if gradient_normalize else 'nognorm'}_"
@@ -90,17 +90,18 @@ def test_lp_config(norm, runs=1, master_seed=1):
                 lr_space = [0.1, 0.5]
             else:
                 lr_space = [0.1, 0.5]
-        for lr, min_lr, llr, C0 in itertools.product(lr_space, [0.01], [0.1], [0.01]):
+        lr_space = [0.01, 0.05, 0.1, 0.2, 0.5] #, 0.1, 0.01]
+        for lr, min_lr, llr, C0 in itertools.product(lr_space, [0.01, 0.001], [0.1], [1.0, 0.1, 0.01]):
             attack_args1 = attack_args0.copy()
             attack_args1.update({
                 'attack_primal_lr': lr,
                 'attack_primal_min_lr': min_lr,
                 'attack_dual_lr': llr,
-                'attack_initial_const': C0 / lr
+                'attack_initial_const': C0
             })
             name1 = name0 + f"lr{lr:.2}_mlr{min_lr:.4}_llr{llr:.2}_C{C0:.4}_"
             for (sampling_radius,
-                 sampling_algorithm) in itertools.product([0.5], ["uniform"]):
+                 sampling_algorithm) in itertools.product([0.1, 0.2], ["uniform"]):
                 attack_args2 = attack_args1.copy()
                 attack_args2.update({
                     'attack_r0_init': sampling_algorithm,
@@ -112,6 +113,8 @@ def test_lp_config(norm, runs=1, master_seed=1):
                     [True, False], [True], [False, True]):
                     # if not lr_decay and norm != 'l0':
                     #     continue
+                    if lr == min_lr and lr_decay:
+                        continue
                     attack_args3 = attack_args2.copy()
                     attack_args3.update({
                         'attack_lr_decay':
@@ -140,6 +143,127 @@ def test_lp_config(norm, runs=1, master_seed=1):
                                     ignored_flags=['attack_use_sign'],
                                     **attack_args3))
 
+
+def test_lp_custom_config(norm, runs=1, master_seed=1):
+    for model in models:
+        type = Path(model).stem.split("_")[-1]
+        working_dir = f"../results/cifar10_10/test_{norm}_{type}"
+        script_module = importlib.import_module("test_optimizer_lp_madry")
+        defined_flags = script_module.FLAGS._flags().keys()
+        export_test_params=[
+            flag for flag in defined_flags if flag.startswith("attack_")]
+        if norm == 'li':
+            export_test_params.append("attack_use_sign")
+        export_test_params.extend(['working_dir', 'seed', 'num_batches', 'batch_size', 'norm'])
+        if not Path(working_dir).exists:
+            continue
+        df = parse_test_optimizer_log(
+            working_dir,
+            export_test_params=export_test_params)
+        if len(df) == 0:
+            continue
+        df = df[getattr(df, f"acc_{norm}") == 0]
+        df = df[df.name.str.contains("_0k")]
+        df = df[df.name.str.contains("_lambd")]
+
+        def get_group_name(name):
+            return "_".join(name.split("_")[5:6])
+
+        df['group_name'] = [get_group_name(n) for n in df['name']]
+        df = df.sort_values(f"{norm}_corr").groupby('group_name', axis=0).head(10)
+        for id, df in df.iterrows():
+            attack_args = {
+                'norm': norm,
+                'num_batches': df.num_batches * df.batch_size // 500,
+                'batch_size': 500, 
+                'seed': df.seed
+            }
+            # load args
+            iterations = df.attack_iterations
+            max_iterations = 10 * iterations
+            loss = df.attack_loss
+            optimizer = df.attack_primal_optimizer
+            gradient_normalize = df.attack_gradient_normalize
+            accelerated = df.attack_accelerated
+            adaptive_momentum = df.attack_adaptive_momentum
+            dual_optimizer = df.attack_dual_optimizer
+            dual_ema = df.attack_dual_ema
+            name = "cifar10_lin_lambd_"
+            name = name + f"{norm}_{type}_{iterations}_{max_iterations // 1000}k_"
+            name = name + f"{optimizer if not accelerated else optimizer + '_apg'}_{loss}_"
+            name = name + f"{'gnorm' if gradient_normalize else 'nognorm'}_"
+            name = name + f"{'adaptive' if adaptive_momentum else 'nonadaptive'}_"
+            name = name + f"{'ema' if dual_ema else 'noema'}_"
+            attack_args.update({
+                'load_from': model,
+                'attack_iterations': iterations,
+                'attack_max_iterations': max_iterations,
+                'attack_loss': loss,
+                'attack_primal_optimizer': optimizer,
+                'attack_gradient_normalize': gradient_normalize,
+                'attack_accelerated': accelerated,
+                'attack_adaptive_momentum': adaptive_momentum,
+                'attack_dual_optimizer': dual_optimizer,
+                'attack_dual_ema': dual_ema,
+                'working_dir': working_dir
+            })
+            if norm == 'li':
+                use_sign = df.attack_use_sign
+                attack_args.update({'attack_use_sign': use_sign})
+                if not gradient_normalize and use_sign:
+                    continue
+                elif gradient_normalize and use_sign:
+                    name = name[:-1] + "sign_"
+
+            lr = df.attack_primal_lr
+            min_lr = df.attack_primal_min_lr
+            llr = df.attack_dual_lr
+            C0 = df.attack_initial_const
+            name = name + f"lr{lr:.2}_mlr{min_lr:.4}_llr{llr:.2}_C{C0:.4}_"
+            attack_args.update({
+                'attack_primal_lr': lr,
+                'attack_primal_min_lr': min_lr,
+                'attack_dual_lr': llr,
+                'attack_initial_const': df.attack_initial_const
+            })
+
+            sampling_radius = df.attack_sampling_radius
+            sampling_algorithm = df.attack_r0_init
+            name = name + f"{sampling_algorithm}_R{sampling_radius}_"
+            attack_args.update({
+                'attack_r0_init': sampling_algorithm,
+                'attack_sampling_radius': sampling_radius
+            })
+
+            lr_decay = df.attack_lr_decay
+            finetune = df.attack_finetune
+            use_proxy = df.attack_use_proxy_constraint
+            name = name + f"{'decay' if lr_decay else 'nodecay'}_"
+            name = name + f"{'finetune' if finetune else 'nofinetune'}_"
+            name = name + f"{'proxy' if use_proxy else 'noproxy'}_"
+            attack_args.update({
+                'attack_lr_decay':
+                lr_decay,
+                'attack_finetune':
+                finetune,
+                'attack_use_proxy_constraint':
+                use_proxy
+            })
+
+            p = [
+                s.name[:-1] for s in list(Path(working_dir).glob("*"))
+            ]
+            if name in p:
+                continue
+            if norm == 'l2' and type == 'linf':
+                print(
+                    generate_test_optimizer_lp(
+                        name=name,
+                        ignored_flags=['attack_use_sign'],
+                        **attack_args))
+
+
 if __name__ == '__main__':
     for norm in ['l0', 'l1', 'l2', 'li']:
-        test_lp_config(norm)
+        # test_lp_config(norm)
+        test_lp_custom_config(norm)
--- mnist/generate_script.py
+++ mnist/generate_script.py
@@ -4,6 +4,7 @@ import importlib
 import itertools
 import subprocess
 from pathlib import Path
+import importlib
 
 import numpy as np
 
@@ -29,64 +30,100 @@ def generate_test_bethge_lp(norm, load_from, **kwargs):
 
 def test_lp_config(norm, runs=1, master_seed=1):
     assert norm in ['l0', 'li', 'l1', 'l2']
-    num_batches = {'l0': 10, 'li': 10, 'l1': 10, 'l2': 5}[norm]
-    attack_args = {'norm': norm, 'num_batches': num_batches}
-    name = "mnist_"
+    num_images = {'l0': 1000, 'li': 1000, 'l1': 1000, 'l2': 500}[norm]
+    batch_size = 500
+    attack_args = {
+        'norm': norm,
+        'num_batches': num_images // batch_size,
+        'batch_size': batch_size
+    }
+    name = "mnist_lin_lambd_c2_"
     for (model, iterations, max_iterations, loss, optimizer, accelerated,
          gradient_normalize,
-         use_sign) in itertools.product(models, [100], [100], ["cw"],
-                                        ["adam", "adagrad", "rmsprop", "sgd"],
-                                        [True, False], [True, False],
+         use_sign,
+         adaptive_momentum,
+         dual_ema) in itertools.product(models, [500], [1], ["cw"],
+                                        ["sgd"], [False, True],
+                                        [False, True],
+                                        [False, True],
+                                        [False],
                                         [True, False]):
+        max_iterations *= iterations
         type = Path(model).stem.split("_")[-1]
-        working_dir = f"../results/mnist_hs/test_{norm}_{type}"
+        working_dir = f"../results/mnist_10/test_{norm}_{type}"
         name0 = name + f"{norm}_{type}_{iterations}_{max_iterations // 1000}k_"
-        if accelerated and optimizer != 'sgd':
-            continue
-        name0 = name0 + f"{optimizer if not accelerated else 'apg_sgd'}_{loss}_"
+        name0 = name0 + f"{optimizer if not accelerated else optimizer + '_apg'}_{loss}_"
         name0 = name0 + f"{'gnorm' if gradient_normalize else 'nognorm'}_"
         attack_args0 = attack_args.copy()
+        if norm == 'li':
+            attack_args0.update({'attack_use_sign': use_sign})
+            if not gradient_normalize:
+                continue
+            elif gradient_normalize and use_sign:
+                name0 = name0[:-1] + "sign_"
+            else:
+                continue
+        elif use_sign:
+            continue
+        if not accelerated and adaptive_momentum:
+            continue
+        if gradient_normalize and norm != 'li':
+            continue
+        name0 = name0 + f"{'adaptive' if adaptive_momentum else 'nonadaptive'}_"
+        name0 = name0 + f"{'ema' if dual_ema else 'noema'}_"
         attack_args0.update({
             'load_from': model,
             'attack_iterations': iterations,
             'attack_max_iterations': max_iterations,
             'attack_loss': loss,
-            'attack_optimizer': optimizer,
-            'attack_accelerated': accelerated,
+            'attack_primal_optimizer': optimizer,
             'attack_gradient_normalize': gradient_normalize,
+            'attack_accelerated': accelerated,
+            'attack_adaptive_momentum': adaptive_momentum,
+            'attack_dual_optimizer': 'sgd',
+            'attack_dual_ema': dual_ema,
             'working_dir': working_dir
         })
-        if norm == 'li':
-            attack_args0.update({'attack_use_sign': use_sign})
-            if not gradient_normalize and use_sign:
-                continue
-            elif gradient_normalize and use_sign:
-                name0 = name0[:-1] + "sign_"
-        elif use_sign:
-            continue
-        if norm in ['li']:
-            lr_space = np.linspace(0.05, 0.5, 10)
+        if type == 'plain':
+            if norm == 'l0':
+                lr_space = [0.1, 0.5]
+            else:
+                lr_space = [0.1, 0.5]
+        elif type == 'l2':
+            if norm == 'li':
+                lr_space = [0.1, 0.5]
+            else:
+                lr_space = [1.0, 0.5]
         else:
-            lr_space = np.linspace(0.1, 1.0, 10)
-        for lr, llr, C0 in itertools.product(lr_space, [0.1], [0.1]):
+            if norm == 'l2':
+                lr_space = [0.1, 0.5]
+            else:
+                lr_space = [0.1, 0.5]
+        lr_space = [0.01, 0.05] + list(np.linspace(0.1, 0.5, 5)) + [1.0]
+        for lr, min_lr, llr, C0 in itertools.product(lr_space, [0.1, 0.01, 0.001], [0.1], [1., 0.1, 0.01]):
+            if lr < min_lr:
+                continue
             attack_args1 = attack_args0.copy()
             attack_args1.update({
                 'attack_primal_lr': lr,
-                'attack_primal_min_lr': lr / 10.0,
+                'attack_primal_min_lr': min_lr,
                 'attack_dual_lr': llr,
                 'attack_initial_const': C0
             })
-            name1 = name0 + f"lr{lr:.2}_llr{llr:.2}_C{C0}_"
-            for sampling_radius, sampling_algorithm in itertools.product(
-                [0.5], ["uniform"]):
+            name1 = name0 + f"lr{lr:.2}_mlr{min_lr:.4}_llr{llr:.2}_C{C0:.4}_"
+            for (sampling_radius,
+                 sampling_algorithm) in itertools.product([0.5], ["uniform"]):
                 attack_args2 = attack_args1.copy()
                 attack_args2.update({
                     'attack_r0_init': sampling_algorithm,
-                    'attack_sampling_radius': sampling_radius
+                    'attack_sampling_radius': sampling_radius,
                 })
                 name2 = name1 + f"{sampling_algorithm}_R{sampling_radius}_"
+                name2 = name2 + "initial_"
                 for lr_decay, finetune, use_proxy in itertools.product(
-                    [True, False], [True], [True, False]):
+                    [True, False], [True], [False]):
+                    if lr == min_lr and lr_decay:
+                        continue
                     attack_args3 = attack_args2.copy()
                     attack_args3.update({
                         'attack_lr_decay':
@@ -107,112 +144,126 @@ def test_lp_config(norm, runs=1, master_seed=1):
                         continue
                     for i in range(runs):
                         seed = np.random.randint(1000)
-                        print(
-                            generate_test_optimizer_lp(
-                                name=name3,
-                                seed=seed,
-                                ignored_flags=['attack_use_sign'],
-                                **attack_args3))
+                        if True:
+                            print(
+                                generate_test_optimizer_lp(
+                                    name=name3,
+                                    seed=seed,
+                                    ignored_flags=['attack_use_sign'],
+                                    **attack_args3))
 
-    df = parse_test_optimizer_log(
-        "../results/mnist_final/test_l2_plain",
+
+def test_lp_custom_config(norm, runs=1, master_seed=1):
+    for model in models:
+        type = Path(model).stem.split("_")[-1]
+        working_dir = f"../results/mnist_10/test_{norm}_{type}"
+        script_module = importlib.import_module("test_optimizer_lp_madry")
+        defined_flags = script_module.FLAGS._flags().keys()
         export_test_params=[
-            "attack_%s" % n for n in [
-                "optimizer", "finetune", "primal_lr", "dual_lr", "r0_init",
-                "sampling_radius", "initial_const", "tol", "max_iter"
+            flag for flag in defined_flags if flag.startswith("attack_")]
+        if norm == 'li':
+            export_test_params.append("attack_use_sign")
+        export_test_params.extend(['seed', 'num_batches', 'batch_size', 'norm'])
+        df = parse_test_optimizer_log(
+            working_dir,
+            export_test_params=export_test_params)
+        df = df[getattr(df, f"acc_{norm}") == 0]
+
+        def get_group_name(name):
+            return "_".join(name.split("_")[5:6])
+
+        df['group_name'] = [get_group_name(n) for n in df['name']]
+        df = df.sort_values(norm).groupby('group_name', axis=0).head(50)
+        df = df[df.name.str.contains("_0k")]
+        for id, df in df.iterrows():
+            attack_args = {
+                'norm': norm,
+                'num_batches': df.num_batches * df.batch_size // 500,
+                'batch_size': 500,
+                'seed': df.seed
+            }
+            # load args
+            iterations = df.attack_iterations
+            max_iterations = 10 * df.attack_max_iterations
+            loss = df.attack_loss
+            optimizer = df.attack_primal_optimizer
+            accelerated = df.attack_accelerated
+            gradient_normalize = df.attack_gradient_normalize
+            name = "mnist_lin_lambd_"
+            name = name + f"{norm}_3_{type}_{iterations}_{max_iterations // 1000}k_"
+            name = name + f"{optimizer if not accelerated else 'apg_sgd'}_{loss}_"
+            name = name + f"{'gnorm' if gradient_normalize else 'nognorm'}_"
+            attack_args.update({
+                'load_from': model,
+                'attack_iterations': iterations,
+                'attack_max_iterations': max_iterations,
+                'attack_loss': loss,
+                'attack_primal_optimizer': optimizer,
+                'attack_dual_optimizer': df.attack_dual_optimizer,
+                'attack_accelerated': accelerated,
+                'attack_gradient_normalize': gradient_normalize,
+                'working_dir': working_dir
+            })
+            if norm == 'li':
+                use_sign = df.attack_use_sign
+                attack_args.update({'attack_use_sign': use_sign})
+                if not gradient_normalize and use_sign:
+                    continue
+                elif gradient_normalize and use_sign:
+                    name = name[:-1] + "sign_"
+
+            lr = df.attack_primal_lr
+            min_lr = df.attack_primal_min_lr
+            llr = df.attack_dual_lr
+            C0 = df.attack_initial_const
+            name = name + f"lr{lr:.2}_mlr{min_lr:.4}_llr{llr:.2}_C{C0}_"
+            attack_args.update({
+                'attack_primal_lr': lr,
+                'attack_primal_min_lr': min_lr,
+                'attack_dual_lr': llr,
+                'attack_initial_const': C0
+            })
+
+            sampling_radius = df.attack_sampling_radius
+            sampling_algorithm = df.attack_r0_init
+            name = name + f"{sampling_algorithm}_R{sampling_radius}_"
+            attack_args.update({
+                'attack_r0_init': sampling_algorithm,
+                'attack_sampling_radius': sampling_radius
+            })
+
+            lr_decay = df.attack_lr_decay
+            finetune = df.attack_finetune
+            use_proxy = df.attack_use_proxy_constraint
+            name = name + f"{'decay' if lr_decay else 'nodecay'}_"
+            name = name + f"{'finetune' if finetune else 'nofinetune'}_"
+            name = name + f"{'proxy' if use_proxy else 'noproxy'}_"
+            attack_args.update({
+                'attack_lr_decay':
+                lr_decay,
+                'attack_finetune':
+                finetune,
+                'attack_use_proxy_constraint':
+                use_proxy
+            })
+
+            p = [
+                s.name[:-1] for s in list(Path(working_dir).glob("*"))
             ]
-        ])
-    df = df[df["acc_l2_1.00"] <= 0.464]
-    df = df[df["attack_max_iter"] == 1000]
-    df = df.sort_values('l2', ascending=True)
-    for model, attack_max_iter in itertools.product(models, [100000]):
-        type = Path(model).stem.split("_")[-1]
-        working_dir = f"../results/mnist_final/test_l2_{type}"
-
-        for i, df_row in df.iterrows():
-            opt = df_row.attack_optimizer
-            lr = df_row.attack_primal_lr
-            lr = 0.05
-            llr = df_row.attack_dual_lr
-            C0 = df_row.attack_initial_const
-            r0_init = df_row.attack_r0_init
-            sampling_radius = int(df_row.attack_sampling_radius)
-            tol = df_row.attack_tol
-            finetune = df_row.attack_finetune
-            finetunestr = "finetune" if finetune else "nofinetune"
-
-            np.random.seed(master_seed)
-            name = f"mnist_l2_{type}_{attack_max_iter // 1000}k_{opt}_lr{lr}_llr{llr}_C{C0}_tol{tol}_r{r0_init}_R{sampling_radius}_{finetunestr}_{hostname}_"
-            p = [s.name[:-1] for s in list(Path(working_dir).glob("*"))]
             if name in p:
                 continue
-            for i in range(runs):
-                seed = np.random.randint(1e6)
+            if norm != 'l2':
                 print(
-                    generate_test_optimizer_l2(
-                        load_from=model,
-                        attack_finetune=finetune,
-                        attack_optimizer=opt,
-                        attack_max_iter=attack_max_iter,
-                        attack_min_iter_per_start=0,
-                        attack_max_iter_per_start=100,
-                        attack_r0_init=r0_init,
-                        attack_sampling_radius=sampling_radius,
-                        attack_primal_lr=lr,
-                        attack_dual_lr=llr,
-                        attack_tol=tol,
-                        attack_initial_const=C0,
-                        num_batches=5,
-                        working_dir=working_dir,
+                    generate_test_optimizer_lp(
                         name=name,
-                        seed=seed))
-
-
-def test_li_config(runs=1, master_seed=1):
-    for (model, max_iter, opt, tol, sampling_radius) in itertools.product(
-            models, [1000], ["adam"], [0.005, 0.001, 0.0005],
-        [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]):
-        type = Path(model).stem.split("_")[-1]
-        if type not in ['linf']:
-            continue
-        working_dir = f"../results/mnist_temp/test_li_{type}"
-        attack_max_iter = max_iter
-        finetune = True
-        finetunestr = "finetune" if finetune else "nofinetune"
-        for lr, llr, r0_init, C0 in itertools.product([0.05], [0.1],
-                                                      ["sign", "uniform"],
-                                                      [0.1, 0.5, 0.9]):
-            np.random.seed(master_seed)
-            name = f"mnist_li_{type}_{attack_max_iter//1000}k_{opt}_lr{lr}_llr{llr}_C{C0}_tol{tol}_r{r0_init}_R{sampling_radius}_{finetunestr}_{hostname}_"
-            p = [s.name[:-1] for s in list(Path(working_dir).glob("*"))]
-            if name in p:
-                continue
-            for i in range(runs):
-                seed = np.random.randint(1e6)
-                print(
-                    generate_test_optimizer_li(
-                        load_from=model,
-                        attack_finetune=finetune,
-                        attack_optimizer=opt,
-                        attack_max_iter=attack_max_iter,
-                        attack_min_iter_per_start=0,
-                        attack_max_iter_per_start=100,
-                        attack_r0_init=r0_init,
-                        attack_sampling_radius=sampling_radius,
-                        attack_primal_lr=lr,
-                        attack_dual_lr=llr,
-                        attack_tol=tol,
-                        attack_initial_const=C0,
-                        num_batches=10,
-                        working_dir=working_dir,
-                        name=name,
-                        seed=seed))
+                        ignored_flags=['attack_use_sign'],
+                        **attack_args))
 
 
 def test_bethge_config(norm, runs=1, master_seed=1):
     assert norm in ['l0', 'li', 'l1', 'l2']
-    num_batches = {'l0': 10, 'li': 10, 'l1': 10, 'l2': 5}[norm]
-    attack_args = {'norm': norm, 'num_batches': num_batches, 'seed': 1}
+    num_images = {'l0': 10, 'li': 10, 'l1': 10, 'l2': 5}[norm]
+    attack_args = {'norm': norm, 'num_images': num_images, 'seed': 1}
     name = "mnist_bethge_"
 
     for model in models:
@@ -229,4 +280,6 @@ def test_bethge_config(norm, runs=1, master_seed=1):
 
 if __name__ == '__main__':
     for norm in ['l0', 'l1', 'l2', 'li']:
-        test_lp_config(norm)
+        if norm == 'l2':
+            # test_lp_custom_config(norm)
+            test_lp_config(norm)
