aa145ac8d2e78df82bcd44eace09431917cd6c8b
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-04-30 01:54:31 +0800

    Update generate script

--- lib/generate_script.py
+++ lib/generate_script.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import six
 
 
-def get_tmpl_str(script_name, ignored_flags, **flags):
+def get_tmpl_str(script_name, **flags):
     """
     script_name: python module where flags are defined
     """
@@ -15,18 +15,17 @@ def get_tmpl_str(script_name, ignored_flags, **flags):
     str_bfr = six.StringIO()
     str_bfr.write("python %(script_name)s.py " % locals())
     for arg_name, arg_value in flags.items():
-        assert arg_name in defined_flags or arg_name in ignored_flags, arg_name
-        str_bfr.write("--%(arg_name)s=%(arg_value)s " % locals())
+        assert arg_name in defined_flags, arg_name
+        if isinstance(arg_value, dict):
+            str_bfr.write("--{}=\"{}\" ".format(arg_name, arg_value))
+        else:
+            str_bfr.write("--{}={} ".format(arg_name, arg_value))
     tmpl_str = str_bfr.getvalue()[:-1]
     return tmpl_str
 
 
 def generate_test_optimizer(script_name,
-                            norm,
-                            load_from,
-                            ignored_flags={},
                             **kwargs):
-    tmpl_str = get_tmpl_str(script_name, ignored_flags, **kwargs)
+    tmpl_str = get_tmpl_str(script_name, **kwargs)
     assert Path(f"{script_name}.py").exists()
-    assert Path(load_from).exists()
-    return "{} --norm={} --load_from={}".format(tmpl_str, norm, load_from)
+    return tmpl_str
