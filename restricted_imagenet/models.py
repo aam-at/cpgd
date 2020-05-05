930c0937a6335d0529caabc91251e313041e302c
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-05-06 02:13:18 +0800

    Added restricted resnet model

--- restricted_imagenet/models.py
+++ restricted_imagenet/models.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 from tensorflow.python.keras import backend
-from tensorflow.python.keras.applications.resnet import ResNet
 
 from lib.utils import add_default_end_points
-from .residual_utils import make_bottleneck_layer
+from residual_utils import make_bottleneck_layer
 
 
 class TsiprasCNN(tf.keras.Model):
@@ -19,21 +18,20 @@ class TsiprasCNN(tf.keras.Model):
 
         # define functional computation graph
         with tf.init_scope():
-            from tensorflow.python.keras.applications.resnet import stack1
-            z = tf.keras.layers.Conv2D(64, 7, stride=2)(x)
+            z = tf.keras.layers.Conv2D(64, 7, strides=2, use_bias=False)(x)
             z = tf.keras.layers.BatchNormalization(axis=bn_axis, epsilon=1e-5)(z)
             z = tf.keras.layers.ReLU()(z)
             z = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='SAME')(z)
-            block1 = make_bottleneck_layer(64, 3, use_bias=False)
+            block1 = make_bottleneck_layer(64, 3, use_bias=False, name="group0")
             z = block1(z)
-            block2 = make_bottleneck_layer(128, 4, use_bias=False)
+            block2 = make_bottleneck_layer(128, 4, use_bias=False, name="group1")
             z = block2(z)
-            block3 = make_bottleneck_layer(256, 6, use_bias=False)
+            block3 = make_bottleneck_layer(256, 6, use_bias=False, name="group2")
             z = block3(z)
-            block4 = make_bottleneck_layer(512, 3, use_bias=False)
+            block4 = make_bottleneck_layer(512, 3, use_bias=False, name="group3")
             z = block4(z)
             z = tf.keras.layers.GlobalAveragePooling2D()(z)
-            logits = tf.keras.layers.Dense(1000)
+            logits = tf.keras.layers.Dense(1000)(z)
         self.model = tf.keras.Model(inputs=x, outputs=[z, logits])
 
     def call(self, inputs, training=True):
