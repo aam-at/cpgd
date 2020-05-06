dad96f5dd6af182ebb54ce81de860ec85f8a3b26
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-05-07 03:16:22 +0800

    Add test optimizer script for imagenet

--- restricted_imagenet/generate_script.py
+++ restricted_imagenet/generate_script.py
@@ -68,12 +68,11 @@ def test_random(runs=1, master_seed=1):
 
 def test_lp_config(attack, runs=1, master_seed=1):
     norm, _ = lp_attacks[attack]
-    num_images = {'l0': 1000, 'li': 1000, 'l1': 1000, 'l2': 500}[norm]
-    batch_size = 500
+    num_images = {'l0': 500, 'li': 500, 'l1': 500, 'l2': 500}[norm]
+    batch_size = 100
     attack_grid_args = {
         'num_batches': [num_images // batch_size],
         'batch_size': [batch_size],
-        'load_from': models,
         'attack': [attack],
         'attack_loss': ["cw"],
         'attack_iterations': [500],
@@ -82,7 +81,7 @@ def test_lp_config(attack, runs=1, master_seed=1):
         'attack_dual_lr': [1e-1],
         'attack_dual_ema': [True],
         'attack_use_proxy_constraint': [False],
-        'attack_loop_number_restarts': [10],
+        'attack_loop_number_restarts': [1],
         'attack_loop_finetune': [True],
         'attack_loop_r0_sampling_algorithm': ['uniform'],
         'attack_loop_r0_sampling_epsilon': [0.5, 0],
@@ -105,80 +104,73 @@ def test_lp_config(attack, runs=1, master_seed=1):
     attack_arg_names = list(attack_grid_args.keys())
     existing_names = []
 
-    for attack_arg_value in itertools.product(*attack_grid_args.values()):
-        model = attack_arg_value[attack_arg_names.index('load_from')]
-        type = Path(model).stem.split("_")[-1]
-        working_dir = f"../results/imagenet_10/test_{type}_{norm}"
-        attack_args = dict(zip(attack_arg_names, attack_arg_value))
-        attack_args.update({
-            'working_dir': working_dir,
-        })
-        if attack_args['attack_loop_r0_sampling_epsilon'] == 0:
-            if type == 'plain':
-                attack_args['attack_loop_r0_sampling_epsilon'] = 0.25
-            else:
-                attack_args['attack_loop_r0_sampling_epsilon'] = 0.4
-        for lr, decay_factor, flr_decay_factor, lr_decay in itertools.product(
-            [0.05, 0.1, 0.5], [0.01], [0.1], [True]):
-            if type != 'plain' and lr == 0.01:
-                continue
-            min_lr = round(lr * decay_factor, 6)
-            if lr_decay and min_lr < lr:
-                lr_config = {
-                    'schedule': 'linear',
-                    'config': {
-                        **LinearDecay(initial_learning_rate=lr,
-                                      minimal_learning_rate=min_lr,
-                                      decay_steps=attack_args['attack_iterations']).get_config(
-                        )
-                    }
-                }
-            else:
-                lr_config = {
-                    'schedule': 'constant',
-                    'config': {
-                        **ConstantDecay(lr).get_config()
-                    }
-                }
-            if lr_decay and flr_decay_factor < 1.0:
-                finetune_lr_config = {
-                    'schedule': 'linear',
-                    'config': {
-                        **LinearDecay(initial_learning_rate=min_lr,
-                                      minimal_learning_rate=round(
-                                          min_lr * flr_decay_factor, 6),
-                                      decay_steps=attack_args['attack_iterations']).get_config(
-                        )
-                    }
-                }
-            else:
-                finetune_lr_config = {
-                    'schedule': 'constant',
-                    'config': {
-                        **ConstantDecay(learning_rate=min_lr).get_config()
-                    }
-                }
+    for type in models.keys():
+        for attack_arg_value in itertools.product(*attack_grid_args.values()):
+            working_dir = f"../results/imagenet_10/test_{type}_{norm}"
+            attack_args = dict(zip(attack_arg_names, attack_arg_value))
             attack_args.update({
-                'attack_loop_lr_config':
-                lr_config,
-                'attack_loop_finetune_lr_config':
-                finetune_lr_config
+                'working_dir': working_dir,
+                'load_from': models[type]
             })
-            base_name = f"imagenet_{type}"
-            if attack == 'l0':
-                base_name += "_new"
-            name = format_name(base_name, attack_args) + '_'
-            attack_args["name"] = name
-            p = [s.name[:-1] for s in list(Path(working_dir).glob("*"))]
-            if name in p or name in existing_names:
-                continue
-            existing_names.append(name)
-            np.random.seed(master_seed)
-            for i in range(runs):
-                seed = np.random.randint(1000)
-                attack_args["seed"] = seed
-                if True:
-                    print(generate_test_optimizer_lp(**attack_args))
+            for lr, decay_factor, flr_decay_factor, lr_decay in itertools.product(
+                [0.005, 0.01, 0.05, 0.1, 0.5], [0.01], [0.1], [True]):
+                if type != 'plain' and lr == 0.01:
+                    continue
+                min_lr = round(lr * decay_factor, 6)
+                if lr_decay and min_lr < lr:
+                    lr_config = {
+                        'schedule': 'linear',
+                        'config': {
+                            **LinearDecay(initial_learning_rate=lr,
+                                          minimal_learning_rate=min_lr,
+                                          decay_steps=attack_args['attack_iterations']).get_config(
+                            )
+                        }
+                    }
+                else:
+                    lr_config = {
+                        'schedule': 'constant',
+                        'config': {
+                            **ConstantDecay(lr).get_config()
+                        }
+                    }
+                if lr_decay and flr_decay_factor < 1.0:
+                    finetune_lr_config = {
+                        'schedule': 'linear',
+                        'config': {
+                            **LinearDecay(initial_learning_rate=min_lr,
+                                          minimal_learning_rate=round(
+                                              min_lr * flr_decay_factor, 6),
+                                          decay_steps=attack_args['attack_iterations']).get_config(
+                            )
+                        }
+                    }
+                else:
+                    finetune_lr_config = {
+                        'schedule': 'constant',
+                        'config': {
+                            **ConstantDecay(learning_rate=min_lr).get_config()
+                        }
+                    }
+                attack_args.update({
+                    'attack_loop_lr_config':
+                    lr_config,
+                    'attack_loop_finetune_lr_config':
+                    finetune_lr_config
+                })
+                base_name = f"imagenet_{type}"
+                name = format_name(base_name, attack_args) + '_'
+                attack_args["name"] = name
+                p = [s.name[:-1] for s in list(Path(working_dir).glob("*"))]
+                if name in p or name in existing_names:
+                    continue
+                existing_names.append(name)
+                np.random.seed(master_seed)
+                for i in range(runs):
+                    seed = np.random.randint(1000)
+                    attack_args["seed"] = seed
+                    if True:
+                        print(generate_test_optimizer_lp(**attack_args))
 
 
 def test_lp_custom_config(attack, topk=3, runs=1, master_seed=1):
