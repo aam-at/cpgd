db879fe9fc2187729c94232bd289d2d8aea3aa35
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-05-08 15:45:53 +0800

    Add gradient l1 attack

--- lib/attack_l1.py
+++ lib/attack_l1.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
-from .attack_lp import ProximalGradientOptimizerAttack
+from .attack_lp import GradientOptimizerAttack, ProximalGradientOptimizerAttack
 from .attack_utils import proximal_l1
 from .utils import l1_metric
 
 
-class ProximalL1Attack(ProximalGradientOptimizerAttack):
+class GradientL1Attack(GradientOptimizerAttack):
+    # TODO: add hard thresholding before projection to improve performance
     def __init__(self, model, **kwargs):
-        super(ProximalL1Attack, self).__init__(model=model, **kwargs)
+        super(GradientL1Attack, self).__init__(model=model, **kwargs)
         self.ord = 1
 
     def gradient_preprocess(self, g):
@@ -16,5 +17,7 @@ class ProximalL1Attack(ProximalGradientOptimizerAttack):
     def lp_metric(self, u, keepdims=False):
         return l1_metric(u, keepdims=keepdims)
 
+
+class ProximalL1Attack(GradientL1Attack, ProximalGradientOptimizerAttack):
     def proximity_operator(self, u, l):
         return proximal_l1(u, l)
