6bba215eb4d9f22f28ba7fdc7fdd31c8cf013405
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-05-13 01:48:30 +0800

    Add mnist thresholds config

--- mnist/test_bethge_attack.py
+++ mnist/test_bethge_attack.py
@@ -11,13 +11,14 @@ import absl
 import numpy as np
 import tensorflow as tf
 from absl import flags
+
+from config import test_thresholds
+from data import load_mnist
 from foolbox.attacks import (DatasetAttack, L0BrendelBethgeAttack,
                              L1BrendelBethgeAttack, L2BrendelBethgeAttack,
                              LinearSearchBlendedUniformNoiseAttack,
                              LinfinityBrendelBethgeAttack)
 from foolbox.models import TensorFlowModel
-
-from data import load_mnist
 from lib.utils import (MetricsDictionary, import_kwargs_as_flags, l0_metric,
                        l1_metric, l2_metric, li_metric, log_metrics,
                        make_input_pipeline, register_experiment_flags,
@@ -80,14 +81,6 @@ def main(unused_args):
         "l1": l1_metric,
         "l2": l2_metric,
         "li": li_metric
-    }
-    test_thresholds = {
-        "l0": np.linspace(1, 100, 100),
-        "l1":
-        [2.0, 2.5, 4.0, 5.0, 6.0, 7.5, 8.0, 8.75, 10.0, 12.5, 16.25, 20.0],
-        "l2": [0.5, 1.0, 1.5, 2.0, 2.5, 3.0],
-        "li":
-        [0.03, 0.05, 0.07, 0.09, 0.1, 0.11, 0.15, 0.2, 0.25, 0.3, 0.325, 0.35],
     }
     # attacks
     attack_kwargs = {
