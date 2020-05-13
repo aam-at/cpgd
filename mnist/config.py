56a76ca49b552dc4b0746c0bf24ef0a44df896d9
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-05-13 12:42:57 +0800

    Update results mnist

--- mnist/config.py
+++ mnist/config.py
@@ -1,9 +1,33 @@
 import numpy as np
 
+test_model_thresholds = {
+    "plain": {
+        "l0": [5, 6, 8, 10, 15],
+        "l1": np.linspace(2, 10, 5),
+        "l2": np.linspace(0.5, 2.5, 5),
+        "li": np.linspace(0.03, 0.11, 5)
+    },
+    "linf": {
+        "l0": np.linspace(2, 10, 5),
+        "l1": np.linspace(2.5, 12.5, 5),
+        "l2": np.linspace(1.0, 3.0, 5),
+        "li": [0.2, 0.25, 0.3, 0.325, 0.35],
+    },
+    "l2": {
+        "l0": np.linspace(5, 25, 5),
+        "l1": np.linspace(5, 20, 5),
+        "l2": np.linspace(1.0, 3.0, 5),
+        "li": np.linspace(0.05, 0.25, 5),
+    }
+}
+
+test_thresholds = {"l0": [], "l1": [], "l2": [], "li": []}
+for model in test_model_thresholds.keys():
+    thresholds = test_model_thresholds[model]
+    for norm in thresholds:
+        test_thresholds[norm].extend(list(np.round(thresholds[norm], 2)))
+
 test_thresholds = {
-    "l0": np.linspace(1, 100, 100),
-    "l1": [2.0, 2.5, 4.0, 5.0, 6.0, 7.5, 8.0, 8.75, 10.0, 12.5, 16.25, 20.0],
-    "l2": [0.5, 1.0, 1.5, 2.0, 2.5, 3.0],
-    "li":
-    [0.03, 0.05, 0.07, 0.09, 0.1, 0.11, 0.15, 0.2, 0.25, 0.3, 0.325, 0.35],
+    norm: sorted(list(set(thresholds)))
+    for norm, thresholds in test_thresholds.items()
 }
