951db780d1ab517b738838e4f9e46e068f3eb6ef
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-06-08 02:48:59 +0800

    Add daa config

--- mnist/generate_script.py
+++ mnist/generate_script.py
@@ -313,6 +313,47 @@ def pgd_config(norm, seed=123):
             print(generate_test_optimizer('test_pgd', **attack_args))
 
 
+def daa_config(seed=123):
+    import test_daa
+    from test_daa import import_flags
+
+    flags.FLAGS._flags().clear()
+    importlib.reload(test_daa)
+    import_flags()
+
+    num_images = 1000
+    batch_size = 500
+    norm = 'li'
+    attack_args = {
+        'num_batches': num_images // batch_size,
+        'batch_size': batch_size,
+        'seed': seed
+    }
+
+    existing_names = []
+    for model in models:
+        type = Path(model).stem.split("_")[-1]
+        for nb_iter, nb_restarts, method, eps, eps_scale in itertools.product(
+                [100], [1, 10, 100], ['dgf', 'blob'], test_model_thresholds[type][norm], [1, 2, 4, 10, 25, 50]):
+            working_dir = f"../results/mnist_daa/test_{type}_{norm}"
+            attack_args.update({
+                'load_from': model,
+                'working_dir': working_dir,
+                'method': method,
+                'attack_nb_restarts': nb_restarts,
+                'attack_nb_iter': nb_iter,
+                'attack_eps': eps,
+                'attack_eps_iter': eps / eps_scale
+            })
+            name = f"mnist_daa_{method}_{type}_n{nb_iter}_N{nb_restarts}_eps{eps}_epss{eps_scale}_"
+            attack_args['name'] = name
+            p = [s.name[:-1] for s in list(Path(working_dir).glob("*"))]
+            if name in p or name in existing_names:
+                continue
+            existing_names.append(name)
+            print(generate_test_optimizer('test_daa', **attack_args))
+
+
 # fab attack
 def fab_config(norm, seed=123):
     import test_fab
