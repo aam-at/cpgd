2948942aa10cbd0f420c889cda0c23c3d53c49ba
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-06-11 13:15:20 +0800

    Add madry cifar10 cnn pytorch model

--- cifar10/models.py
+++ cifar10/models.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+import torch
 from absl import flags
+from torch import nn
+from torch.nn import functional as F
 
 from lib.utils import add_default_end_points, change_default_args
 
@@ -49,3 +52,34 @@ class MadryCNN(tf.keras.Model):
             inputs = inputs - tf.ones_like(inputs) * 0.5
         h, logits = self.model(inputs, training=training)
         return add_default_end_points({'features': h, 'logits': logits})
+
+
+class MadryCNNPt(torch.nn.Module):
+    # Model trained using adversarial training with projected gradient attack
+    def __init__(self, model_type='plain'):
+        super(MadryCNNPt, self).__init__()
+        use_bias = model_type == 'plain'
+        self.c1 = nn.Conv2d(3, 96, 3, padding=1, bias=use_bias)
+        self.c2 = nn.Conv2d(96, 96, 3, padding=1, bias=use_bias)
+        self.c3 = nn.Conv2d(96, 192, 3, padding=1, stride=2, bias=use_bias)
+        self.c4 = nn.Conv2d(192, 192, 3, padding=1, bias=use_bias)
+        self.c5 = nn.Conv2d(192, 192, 3, padding=1, bias=use_bias)
+        self.c6 = nn.Conv2d(192, 192, 3, padding=1, stride=2, bias=use_bias)
+        self.c7 = nn.Conv2d(192, 192, 3, padding=1, bias=use_bias)
+        self.c7 = nn.Conv2d(192, 384, 3, padding=1, stride=2, bias=use_bias)
+        self.fc1 = nn.Linear(6144, 1200)
+        self.fc2 = nn.Linear(1200, 10)
+
+    def forward(self, x):
+        o = F.relu(self.c1(x))
+        o = F.relu(self.c2(x))
+        o = F.relu(self.c3(x))
+        o = F.relu(self.c4(x))
+        o = F.relu(self.c5(x))
+        o = F.relu(self.c6(x))
+        o = F.relu(self.c7(x))
+        # permute to be compatible with tensorflow
+        o = o.permute(0, 2, 3, 1)
+        o = o.reshape(x.shape[0], -1)
+        o = F.relu(self.fc1(o))
+        return self.fc2(o)
