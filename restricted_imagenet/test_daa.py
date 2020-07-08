098b9ae7f9389ab6f929b44af31c633ec585038f
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-07-08 18:42:53 +0800

    Fix threshold formatting for imagenet

--- restricted_imagenet/test_daa.py
+++ restricted_imagenet/test_daa.py
@@ -159,7 +159,7 @@ def main(unused_args):
         # robust accuracy at threshold
         for threshold in test_thresholds["li"]:
             is_adv_at_th = tf.logical_and(li <= threshold + 5e-6, is_adv)
-            test_metrics["acc_li_%.3f" % threshold](~is_adv_at_th)
+            test_metrics["acc_li_%.4f" % threshold](~is_adv_at_th)
         test_metrics["success_rate"](is_adv[is_corr])
 
     # reset metrics
--- restricted_imagenet/test_fab.py
+++ restricted_imagenet/test_fab.py
@@ -134,7 +134,7 @@ def main(unused_args):
         # robust accuracy at threshold
         for threshold in test_thresholds[f"{FLAGS.attack_norm}"]:
             is_adv_at_th = torch.logical_and(lp <= threshold, is_adv)
-            test_metrics[f"acc_{FLAGS.attack_norm}_%.3f" %
+            test_metrics[f"acc_{FLAGS.attack_norm}_%.4f" %
                          threshold](~is_adv_at_th)
         test_metrics["success_rate"](is_adv[is_corr])
 
