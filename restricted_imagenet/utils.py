4a3cef77304fdd5184bf4a0953f14c8534988972
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-07-08 15:11:30 +0800

    Add functions to load pytorch imagenet model

--- restricted_imagenet/utils.py
+++ restricted_imagenet/utils.py
@@ -1,4 +1,8 @@
+import functools
+import itertools
+
 import tensorflow as tf
+import torch
 from tensorflow.python.training import py_checkpoint_reader
 
 
@@ -46,6 +50,79 @@ def load_tsipras(load_from, model_vars):
             all_ckpt_tensors[mapped_var_name] = True
         except:
             print("Failed to find: {}".format(mapped_var_name))
+
+    assert all([v for v in initialized_vars.values()]), "Failed to load model"
+    print("Failed to find a matching variable ckpt -> model: {}".format(
+        [name for name, v in all_ckpt_tensors.items() if not v]))
+
+
+def map_var_name_pt(var_name):
+    var_name = functools.reduce(
+        lambda n, i: n.replace("backbone.layer%d.%d" %
+                               (i[0], i[1]), "group%d/block%d" %
+                               (i[0] - 1, i[1])),
+        itertools.product(range(10), range(10)), var_name)
+    var_name = var_name.replace("backbone.", "")
+    var_name = functools.reduce(
+        lambda n, i: n.replace("block%d.bn%d" %
+                               (i[0], i[1]), "block%d/conv%d/bn" %
+                               (i[0], i[1])),
+        itertools.product(range(10), range(10)), var_name)
+    var_name = functools.reduce(
+        lambda n, i: n.replace("block%d.conv%d" %
+                               (i[0], i[1]), "block%d/conv%d" % (i[0], i[1])),
+        itertools.product(range(10), range(10)), var_name)
+    # handle dense layers naming
+    var_name = var_name.replace("fc.weight", "linear/W")
+    var_name = var_name.replace("fc.bias", "linear/b")
+    # handle convolutional layers naming
+    var_name = functools.reduce(
+        lambda n, i: n.replace("/conv%d.weight" % i, "/conv%d/W" % i),
+        range(10), var_name)
+    var_name = var_name.replace("conv1.weight", "conv0/W")
+    var_name = var_name.replace(".downsample.0", "/convshortcut")
+    var_name = var_name.replace(".downsample.1", "/convshortcut/bn")
+    # handle batch normalization layers naming
+    var_name = var_name.replace("bn1", "conv0/bn")
+    var_name = var_name.replace("bn.weight", "bn/gamma")
+    var_name = var_name.replace("bn.bias", "bn/beta")
+    var_name = var_name.replace(".running_var", "/variance/EMA")
+    var_name = var_name.replace(".running_mean", "/mean/EMA")
+    # handle remaining naming issues
+    var_name = var_name.replace(".weight", "/W")
+    return var_name
+
+
+def load_tsipras_pt(load_from, model_params):
+    ckpt_manager = tf.train.CheckpointManager(tf.train.Checkpoint(),
+                                              load_from,
+                                              max_to_keep=3)
+    ckpt_reader = py_checkpoint_reader.NewCheckpointReader(
+        ckpt_manager.latest_checkpoint)
+    all_ckpt_tensors = ckpt_reader.get_variable_to_shape_map()
+    all_ckpt_tensors = {
+        name: name.endswith("Momentum") or name.startswith("EMA")
+        for name in all_ckpt_tensors
+    }
+    model_params = {
+        key: value
+        for key, value in model_params.items()
+        if "num_batches_tracked" not in key
+    }
+    initialized_vars = {param_name: False for param_name in model_params}
+    for param_name, param in model_params.items():
+        mapped_param_name = map_var_name_pt(param_name)
+        try:
+            param_loaded_value = ckpt_reader.get_tensor(mapped_param_name)
+            if param_loaded_value.ndim == 4:
+                param_loaded_value = param_loaded_value.transpose(3, 2, 0, 1)
+            elif param_loaded_value.ndim == 2:
+                param_loaded_value = param_loaded_value.transpose(1, 0)
+            param.data.copy_(torch.from_numpy(param_loaded_value))
+            initialized_vars[param_name] = True
+            all_ckpt_tensors[mapped_param_name] = True
+        except:
+            print("Failed to find: {}".format(mapped_param_name))
 
     assert all([v for v in initialized_vars.values()]), "Failed to load model"
     print("Failed to find a matching variable ckpt -> model: {}".format(
