d7f33c9cc297681f718e6cb7ef709067d3a08b11
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-11-09 00:31:14 +0800

    Update l2 attack

--- lib/attack_l2.py
+++ lib/attack_l2.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
-from .attack_lp import GradientOptimizerAttack, ProximalGradientOptimizerAttack
+from .attack_lp import (ClassConstrainedAttack, NormConstrainedAttack,
+                        PrimalDualGradientAttack,
+                        ProximalPrimalDualGradientAttack)
 from .attack_utils import proximal_l2
 from .tf_utils import l2_metric
 
 
-class GradientL2Attack(GradientOptimizerAttack):
+class L2Attack(PrimalDualGradientAttack):
     def __init__(self, model, **kwargs):
-        super(GradientL2Attack, self).__init__(model=model, **kwargs)
+        super(L2Attack, self).__init__(model=model, **kwargs)
         self.ord = 2
 
     def gradient_preprocess(self, g):
@@ -18,6 +20,23 @@ class GradientL2Attack(GradientOptimizerAttack):
         return l2_metric(u, keepdims=keepdims)
 
 
-class ProximalL2Attack(GradientL2Attack, ProximalGradientOptimizerAttack):
+class ClassConstrainedL2Attack(ClassConstrainedAttack, L2Attack):
+    pass
+
+
+class NormConstrainedL2Attack(NormConstrainedAttack, L2Attack):
+    pass
+
+
+class ProximalL2Attack(L2Attack, ProximalPrimalDualGradientAttack):
     def proximity_operator(self, u, l):
         return proximal_l2(u, l)
+
+
+class ClassConstrainedProximalL2Attack(ClassConstrainedAttack,
+                                       ProximalL2Attack):
+    pass
+
+
+class NormConstrainedProximalL2Attack(NormConstrainedAttack, ProximalL2Attack):
+    pass
