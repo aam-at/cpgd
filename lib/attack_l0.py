88650720ed897aa252a41f55a0abc66cd4a9255f
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-11-09 00:27:49 +0800

    Update l0 attack

--- lib/attack_l0.py
+++ lib/attack_l0.py
@@ -2,12 +2,13 @@ from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
 
-from .attack_lp import ProximalGradientOptimizerAttack
+from .attack_lp import (ClassConstrainedAttack, NormConstrainedAttack,
+                        ProximalPrimalDualGradientAttack)
 from .attack_utils import proximal_l0
 from .tf_utils import l0_pixel_metric
 
 
-class ProximalL0Attack(ProximalGradientOptimizerAttack):
+class ProximalL0Attack(ProximalPrimalDualGradientAttack):
     def __init__(self, model, channel_dim=-1, **kwargs):
         super(ProximalL0Attack, self).__init__(model=model, **kwargs)
         self.channel_dim = channel_dim
@@ -19,7 +20,9 @@ class ProximalL0Attack(ProximalGradientOptimizerAttack):
     def lp_metric(self, u, keepdims=False):
         """Compute l0 metric pixelwise (excluding channel dimension)
         """
-        return l0_pixel_metric(u, channel_dim=self.channel_dim, keepdims=keepdims)
+        return l0_pixel_metric(u,
+                               channel_dim=self.channel_dim,
+                               keepdims=keepdims)
 
     def proximity_operator(self, u, l):
         """Compute l0 proximal operator pixelwise (excluding channel dimension)
@@ -27,3 +30,12 @@ class ProximalL0Attack(ProximalGradientOptimizerAttack):
         u_c = tf.reduce_max(tf.abs(u), axis=self.channel_dim, keepdims=True)
         pu_c = proximal_l0(u_c, l)
         return tf.where(tf.abs(pu_c) > 0, u, 0.0)
+
+
+class NormConstrainedProximalL0Attack(NormConstrainedAttack, ProximalL0Attack):
+    pass
+
+
+class ClassConstrainedProximalL0Attack(ClassConstrainedAttack,
+                                       ProximalL0Attack):
+    pass
