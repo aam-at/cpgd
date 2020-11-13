9dedd04e0cf4cb0c8b4acfee8cd3777a225a9824
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-11-13 11:17:47 +0800

    Update test bethge script

--- mnist/test_bethge.py
+++ mnist/test_bethge.py
@@ -14,14 +14,14 @@ from foolbox.attacks import (DatasetAttack, L0BrendelBethgeAttack,
                              LinearSearchBlendedUniformNoiseAttack,
                              LinfinityBrendelBethgeAttack)
 from foolbox.models import TensorFlowModel
-
-from config import test_thresholds
-from data import load_mnist
 from lib.tf_utils import (MetricsDictionary, l0_metric, l1_metric, l2_metric,
                           li_metric, make_input_pipeline)
 from lib.utils import (import_klass_annotations_as_flags, log_metrics,
                        register_experiment_flags, reset_metrics,
                        setup_experiment)
+
+from config import test_thresholds
+from data import load_mnist
 from models import MadryCNNTf
 from utils import load_madry
 
@@ -47,7 +47,8 @@ lp_attacks = {
 def import_flags(norm):
     global lp_attacks
     assert norm in lp_attacks
-    import_klass_annotations_as_flags(lp_attacks[norm], "attack_")
+    import_klass_annotations_as_flags(lp_attacks[norm], "attack_",
+                                      exclude_args=['init_attack', 'tensorboard'])
 
 
 def main(unused_args):
