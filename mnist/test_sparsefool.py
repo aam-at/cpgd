ec49492c80d5efa1fdaff6b7edc13aeae10758ba
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-11-13 11:17:32 +0800

    Update test sparsefool with pytorch

--- mnist/test_sparsefool.py
+++ mnist/test_sparsefool.py
@@ -5,13 +5,9 @@ import sys
 import time
 
 import absl
-import tensorflow as tf
 import torch
 import torch.nn.functional as F
 from absl import flags
-
-from config import test_thresholds
-from data import load_mnist
 from lib.pt_utils import (MetricsDictionary, l0_metric, l1_metric, l2_metric,
                           li_metric, to_torch)
 from lib.sparsefool import sparsefool
@@ -19,6 +15,9 @@ from lib.tf_utils import limit_gpu_growth, make_input_pipeline
 from lib.utils import (import_func_annotations_as_flags, log_metrics,
                        register_experiment_flags, reset_metrics,
                        setup_experiment)
+
+from config import test_thresholds
+from data import load_mnist
 from models import MadryCNNPt
 from utils import load_madry_pt
 
@@ -41,15 +40,6 @@ def main(unused_args):
     assert FLAGS.load_from is not None
     setup_experiment(f"madry_sparsefool_test", [__file__])
 
-    # data
-    _, _, test_ds = load_mnist(FLAGS.validation_size,
-                               data_format="NCHW",
-                               seed=FLAGS.data_seed)
-    test_ds = tf.data.Dataset.from_tensor_slices(test_ds)
-    test_ds = make_input_pipeline(test_ds,
-                                  shuffle=False,
-                                  batch_size=FLAGS.batch_size)
-
     # models
     num_classes = 10
     classifier = MadryCNNPt(wrap_outputs=False)
@@ -58,6 +48,17 @@ def main(unused_args):
     load_madry_pt(FLAGS.load_from, classifier.parameters())
     classifier.cuda()
     classifier.eval()
+
+    # data
+    _, _, test_ds = load_mnist(FLAGS.validation_size,
+                               data_format="NCHW",
+                               seed=FLAGS.data_seed)
+    # NOTE: load tensorflow after converting model to cuda
+    import tensorflow as tf
+    test_ds = tf.data.Dataset.from_tensor_slices(test_ds)
+    test_ds = make_input_pipeline(test_ds,
+                                  shuffle=False,
+                                  batch_size=FLAGS.batch_size)
 
     # attacks
     attack_kwargs = {
