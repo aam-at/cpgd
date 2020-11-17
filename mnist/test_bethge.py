86825e5f2e3a261992d0fdf83b23e257106b968b
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-11-17 13:20:05 +0800

    Update bethge float formatting

--- mnist/test_bethge.py
+++ mnist/test_bethge.py
@@ -16,8 +16,8 @@ from foolbox.attacks import (DatasetAttack, L0BrendelBethgeAttack,
 from foolbox.models import TensorFlowModel
 from lib.tf_utils import (MetricsDictionary, l0_metric, l1_metric, l2_metric,
                           li_metric, make_input_pipeline)
-from lib.utils import (import_klass_annotations_as_flags, log_metrics,
-                       register_experiment_flags, reset_metrics,
+from lib.utils import (format_float, import_klass_annotations_as_flags,
+                       log_metrics, register_experiment_flags, reset_metrics,
                        setup_experiment)
 
 from config import test_thresholds
@@ -173,7 +173,7 @@ def main(unused_args):
         # robust accuracy at threshold
         for threshold in test_thresholds[FLAGS.norm]:
             is_adv_at_th = tf.logical_and(lp <= threshold, is_adv)
-            test_metrics[f"acc_{FLAGS.norm}_%.2f" % threshold](~is_adv_at_th)
+            test_metrics[f"acc_{FLAGS.norm}_%s" % format_float(threshold)](~is_adv_at_th)
         test_metrics["success_rate"](is_adv[is_corr])
 
         return image_adv
