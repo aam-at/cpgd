db4ca93401507950533d378f225815b0aef77a6c
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-12-08 01:06:50 +0800

    Enable different operators for l0 norm minimization

--- lib/attack_l0.py
+++ lib/attack_l0.py
@@ -1,18 +1,17 @@
 from __future__ import absolute_import, division, print_function
 
-import tensorflow as tf
-
 from .attack_lp import (ClassConstrainedAttack, NormConstrainedAttack,
                         ProximalPrimalDualGradientAttack)
-from .attack_utils import proximal_l0, proximal_l1
+from .attack_utils import proximal_l0, proximal_l1, proximal_l12, proximal_l23
 from .tf_utils import l0_pixel_metric
 
 
 class ProximalL0Attack(ProximalPrimalDualGradientAttack):
-    def __init__(self, model, soft_threshold=False, channel_dim=-1, **kwargs):
+    def __init__(self, model, operator="l0", channel_dim=-1, **kwargs):
         super(ProximalL0Attack, self).__init__(model=model, **kwargs)
+        assert operator in ["l0", "l1/2", "l2/3", "l1"]
         self.channel_dim = channel_dim
-        self.soft_threshold = soft_threshold
+        self.operator = operator
         self.ord = 0
 
     def gradient_preprocess(self, g):
@@ -28,13 +27,13 @@ class ProximalL0Attack(ProximalPrimalDualGradientAttack):
     def proximity_operator(self, u, l):
         """Compute l0 proximal operator pixelwise (excluding channel dimension)
         """
-        if self.soft_threshold:
-            u = proximal_l1(u, l)
-            return u
-        else:
-            u_c = tf.reduce_max(tf.abs(u), axis=self.channel_dim, keepdims=True)
-            pu_c = proximal_l0(u_c, l)
-            return tf.where(tf.abs(pu_c) > 0, u, 0.0)
+        operators = {
+            "l0": proximal_l0,
+            "l1/2": proximal_l12,
+            "l2/3": proximal_l23,
+            "l1": proximal_l1,
+        }
+        return operators[self.operator](u, l)
 
 
 class NormConstrainedProximalL0Attack(NormConstrainedAttack, ProximalL0Attack):
