c621b59f9dfa724eb2299134a2f907b28cd16cdc
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-12-11 17:33:38 +0800

    Update cifar10 pgd script

--- cifar10/test_pgd.py
+++ cifar10/test_pgd.py
@@ -103,6 +103,7 @@ def main(unused_args):
     attack_kwargs = {
         kwarg.replace("attack_", ""): getattr(FLAGS, kwarg)
         for kwarg in dir(FLAGS) if kwarg.startswith("attack_")
+        and kwarg not in ['attack_loss', 'attack_nb_restarts']
     }
     if FLAGS.norm != 'l1':
         attack_kwargs['ord'] = 2 if FLAGS.norm == 'l2' else np.inf
