16aeb66cbf5323ed8073f0557a679e7639b6b314
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-12-14 03:41:39 +0800

    Update pixel l0 attack

--- lib/attack_l0.py
+++ lib/attack_l0.py
@@ -2,18 +2,26 @@ from __future__ import absolute_import, division, print_function
 
 import functools
 
+import tensorflow as tf
+
 from .attack_lp import (ClassConstrainedAttack, NormConstrainedAttack,
                         ProximalPrimalDualGradientAttack)
 from .attack_utils import proximal_l0, proximal_l1, proximal_l12, proximal_l23
-from .tf_utils import l0_pixel_metric
+from .tf_utils import l0_metric, l0_pixel_metric
 
 
 class ProximalL0Attack(ProximalPrimalDualGradientAttack):
-    def __init__(self, model, operator="l0", channel_dim=-1, **kwargs):
+    def __init__(self,
+                 model,
+                 operator="l0",
+                 pixel_attack=True,
+                 channel_dim=-1,
+                 **kwargs):
         super(ProximalL0Attack, self).__init__(model=model, **kwargs)
         assert operator in ["l0", "l1/2", "l2/3", "l1"]
-        self.channel_dim = channel_dim
         self.operator = operator
+        self.pixel_attack = pixel_attack
+        self.channel_dim = channel_dim
         self.ord = 0
 
     def gradient_preprocess(self, g):
@@ -22,9 +30,12 @@ class ProximalL0Attack(ProximalPrimalDualGradientAttack):
     def lp_metric(self, u, keepdims=False):
         """Compute l0 metric pixelwise (excluding channel dimension)
         """
-        return l0_pixel_metric(u,
-                               channel_dim=self.channel_dim,
-                               keepdims=keepdims)
+        if self.pixel_attack:
+            return l0_pixel_metric(u,
+                                   channel_dim=self.channel_dim,
+                                   keepdims=keepdims)
+        else:
+            return l0_metric(u, keepdims=keepdims)
 
     def proximity_operator(self, u, l):
         """Compute l0 proximal operator pixelwise (excluding channel dimension)
@@ -35,7 +46,19 @@ class ProximalL0Attack(ProximalPrimalDualGradientAttack):
             "l2/3": functools.partial(proximal_l23, has_ecc=self.has_ecc),
             "l1": proximal_l1,
         }
-        return operators[self.operator](u, l)
+        operator = operators[self.operator]
+        if self.pixel_attack:
+            # apply operator on the largest channel
+            # 1) if the largest channel becomes zero, zero the rest of the elements
+            # 2) else set the elements to the original values unless it is maximum
+            max_indices = tf.argmax(tf.abs(u), axis=self.channel_dim)
+            u_c = tf.expand_dims(tf.gather(u, max_indices, batch_dims=3), -1)
+            pu_c = operator(u_c, l)
+            o = tf.where(tf.abs(pu_c) > 0, u, 0.0)
+            out = tf.where(o == u_c, pu_c, o)
+            return out
+        else:
+            return operator(u, l)
 
 
 class NormConstrainedProximalL0Attack(NormConstrainedAttack, ProximalL0Attack):
