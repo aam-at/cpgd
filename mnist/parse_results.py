02fe5172c7d68f64f071f13772582891202d4d2b
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-12-14 01:56:17 +0800

    Update results mnist script

---
+++ mnist/parse_results.py
@@ -0,0 +1,170 @@
+import glob
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from lib.parse_logs import parse_log
+from lib.utils import format_float
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
+from config import test_model_thresholds
+
+
+def parse_results(model_type,
+                  norm,
+                  attack,
+                  base_dir="results",
+                  sort_column=None,
+                  group_by=None,
+                  extra_params=None):
+    if sort_column is None:
+        sort_column = f"{norm}_corr"
+    if extra_params is None:
+        extra_params = []
+    if group_by is not None:
+        extra_params.append(group_by)
+    extra_params.extend(["num_batches", "batch_size"])
+
+    model_thresholds = [
+        format_float(th)
+        for th in np.array(test_model_thresholds[model_type][norm])
+    ]
+    dirs = glob.glob(f"{base_dir}/test_{model_type}/{norm}/{attack}/*")
+    df_logs = []
+    for load_dir in dirs:
+        df_logs.append(
+            parse_log(load_dir,
+                      exclude=["nll_loss", "conf"],
+                      export_test_params=extra_params))
+    if len(df_logs) == 0:
+        raise ValueError("No logs parsed")
+    df = pd.concat(df_logs, ignore_index=True)
+    df = df.sort_values(sort_column)
+    df["total"] = df["num_batches"] * df["batch_size"]
+    df = df.drop(columns=["num_batches", "batch_size"])
+
+    def col_sort(k):
+        if k == "name":
+            return 1000
+        elif k.startswith("acc"):
+            return 100
+        elif k.startswith(f"{norm}"):
+            return 10
+        else:
+            return 0
+
+    df = df[sorted(df.columns, key=col_sort, reverse=True)]
+
+    if len(model_thresholds) > 0:
+        for col in df.columns:
+            if col.startswith(f"acc_{norm}_"):
+                threshold = col.replace(f"acc_{norm}_", "")
+                if threshold not in model_thresholds:
+                    df = df.drop(columns=[col])
+
+    for col in df.columns:
+        if col.startswith(f"acc_{norm}_"):
+            df[col] = df[col] * 100
+    return df
+
+
+def output_excel(df,
+                 model_type,
+                 norm,
+                 attack,
+                 base_dir="results",
+                 group_by=None):
+    df_list = {}
+    if group_by is None:
+        df_list[attack] = df
+    else:
+        df_gr = df.groupby(by=group_by)
+        for gr_key, gr_df in df_gr:
+            gr_df = gr_df.drop(columns=group_by)
+            df_list[f"{attack}-{gr_key}"] = gr_df
+
+    output_file = f"{base_dir}/{model_type}_{norm}.xlsx"
+    if Path(output_file).exists():
+        writer = pd.ExcelWriter(output_file, mode='a')
+        book = load_workbook(output_file)
+        if attack in book.sheetnames:
+            del book[attack]
+        writer.book = book
+    else:
+        writer = pd.ExcelWriter(output_file, mode='w')
+
+    book = writer.book
+    for name, df in df_list.items():
+        if name in book.sheetnames:
+            del book[name]
+        df.to_excel(writer, sheet_name=name)
+        # autoresize columns
+        ws = writer.sheets[name]
+        dims = {}
+        for row in ws.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column] = max(
+                        (dims.get(cell.column, 0), len(str(cell.value))))
+        for col, value in dims.items():
+            ws.column_dimensions[get_column_letter(col)].width = value
+
+    def sort_by_title(title):
+        if norm == 'li':
+            index = {'df': 0, 'bethge': 1, 'daa': 2, 'pgd': 3, 'fab': 4}
+        elif norm == 'l2':
+            index = {
+                'df': 0,
+                'cw': 1,
+                'ddn': 2,
+                'bethge': 3,
+                'pgd': 4,
+                'fab': 5
+            }
+        elif norm == 'l1':
+            index = {
+                'sparsefool': 0,
+                'ead': 1,
+                'bethge': 2,
+                'pgd': 3,
+                'fab': 4
+            }
+        elif norm == 'l0':
+            index = {'jsma': 0, 'pixel': 1, 'bethge': 2, 'cornersearch': 3}
+        else:
+            raise ValueError
+        for n, v in index.items():
+            if n in title:
+                return v
+        return 100
+
+    book._sheets.sort(key=lambda ws: sort_by_title(ws.title))
+    writer.save()
+
+    now = datetime.now()
+    today = datetime.today()
+    print(f"Wrote {attack}-{norm} on {model_type} at {today}")
+
+
+if __name__ == '__main__':
+    base_dir = "../results_mnist"
+    df = parse_results(base_dir, "plain", "li", "our_li", base_dir=base_dir)
+    output_excel(df, "plain", "li", "our_li", base_dir=base_dir)
+    # for model in models:
+    #     type = Path(model).stem.split("_")[-1]
+    #     for norm in ["li", "l2", "l1", "l0"]:
+    #         if norm == "li":
+    #             attacks = ['df', 'bethge', 'daa', 'pgd', 'fab', 'our_li']
+    #         elif norm == "l2":
+    #             attacks = ['df', 'cw', 'ddn', 'bethge', 'pgd', 'fab', 'our_l2']
+    #         elif norm == "l1":
+    #             attacks = [
+    #                 'sparsefool', 'ead', 'bethge', 'pgd', 'fab', 'our_l1'
+    #             ]
+    #         elif norm == "l0":
+    #             attacks = ['jsma', 'pixel', 'bethge', 'cornersearch', 'our_l0']
+    #         for attack in [f"our_{norm}"]:
+    #             df = parse_results(base_dir, "plain", "li", "our_li")
+    #             output_excel(df, base_dir, "plain", "li", "our_li")
