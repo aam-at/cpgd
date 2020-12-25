e5d89169e86fab4f547e4148bf46ff2470be4ddc
Alexander Matyasko <alexander.matyasko@gmail.com>
2020-12-25 19:00:13 +0800

    Update residual model for pytorch and tensorflow

--- restricted_imagenet/models.py
+++ restricted_imagenet/models.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 import torch
-from lib.residual_utils import make_bottleneck_layer
-from tensorflow.python.keras import backend
-from torchvision.models.resnet import resnet50
+from lib.residual_utils import ResNetCNNPt, ResnetCNNTf
 
 
 class TsiprasCNN(tf.keras.Model):
@@ -21,7 +19,7 @@ class TsiprasCNN(tf.keras.Model):
     # Imagenet robust model Tsipras et al
     def __init__(self):
         super(TsiprasCNN, self).__init__()
-        self.backbone = ResnetCNN()
+        self.backbone = ResnetCNNTf()
 
     @staticmethod
     def image_preprocess(image, bgr=True):
@@ -43,65 +41,6 @@ class TsiprasCNN(tf.keras.Model):
         return add_default_end_points({"logits": logits[:, :num_labels]})
 
 
-class ResnetCNN(tf.keras.Model):
-    def __init__(self):
-        super(ResnetCNN, self).__init__()
-
-    def build(self, inputs_shape):
-        # configure inputs
-        x_shape = inputs_shape
-        x = tf.keras.layers.Input(shape=x_shape[1:], name="x")
-        bn_axis = 3 if backend.image_data_format() == "channels_last" else 1
-
-        # define functional computation graph
-        with tf.init_scope():
-            z = tf.keras.layers.Conv2D(64,
-                                       7,
-                                       strides=2,
-                                       use_bias=False,
-                                       padding="SAME",
-                                       activation=None,
-                                       name="conv0")(x)
-            z = tf.keras.layers.BatchNormalization(axis=bn_axis,
-                                                   epsilon=1e-5,
-                                                   name="conv0/bn")(z)
-            z = tf.keras.layers.ReLU()(z)
-            z = tf.keras.layers.MaxPool2D(pool_size=3,
-                                          strides=2,
-                                          padding="SAME")(z)
-            block1 = make_bottleneck_layer(64,
-                                           3,
-                                           use_bias=False,
-                                           stride=1,
-                                           name="group0")
-            z = block1(z)
-            block2 = make_bottleneck_layer(128,
-                                           4,
-                                           use_bias=False,
-                                           stride=2,
-                                           name="group1")
-            z = block2(z)
-            block3 = make_bottleneck_layer(256,
-                                           6,
-                                           use_bias=False,
-                                           stride=2,
-                                           name="group2")
-            z = block3(z)
-            block4 = make_bottleneck_layer(512,
-                                           3,
-                                           use_bias=False,
-                                           stride=2,
-                                           name="group3")
-            z = block4(z)
-            z = tf.keras.layers.GlobalAveragePooling2D()(z)
-            logits = tf.keras.layers.Dense(1000)(z)
-        self.model = tf.keras.Model(inputs=x, outputs=logits)
-
-    def call(self, inputs, training=True):
-        logits = self.model(inputs, training=training)
-        return logits
-
-
 class TsiprasCNNPt(torch.nn.Module):
     LABEL_RANGES = [
         (151, 268),
@@ -118,7 +57,7 @@ class TsiprasCNNPt(torch.nn.Module):
     # Imagenet robust model Tsipras et al
     def __init__(self, data_format="NHWC", wrap_outputs=True):
         super(TsiprasCNNPt, self).__init__()
-        self.backbone = resnet50(pretrained=False)
+        self.backbone = ResNetCNNPt()
         self.data_format = data_format
         self.wrap_outputs = wrap_outputs
 
