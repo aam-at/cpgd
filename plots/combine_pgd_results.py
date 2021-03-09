59f3d07978a258cef4a777d464752dc5e27bc356
Alexander Matyasko <alexander.matyasko@gmail.com>
2021-03-09 22:15:16 +0000

    Update combine pgd results script

--- plots/combine_pgd_results.py
+++ plots/combine_pgd_results.py
@@ -14,29 +14,48 @@ class DeepDict(defaultdict):
 
 
 ROOT = "../results_mnist/"
-results = DeepDict(DeepDict(DeepDict(list)))
-shape = (501, 1000)
+results = DeepDict(DeepDict(DeepDict(DeepDict(list))))
 for type, norm in itertools.product(["plain", "linf", "l2"],
                                     ["l0", "l1", "l2", "li"]):
     base_dir = Path(ROOT) / f"test_{type}" / norm / "pgd"
     load_dirs = list(base_dir.glob("*pgd_acc*"))
-    for load_dir in load_dirs:
+    for load_dir in sorted(list(load_dirs)):
         f = list(load_dir.glob("*.npy"))[0]
-        results[norm][type][f.stem].append(np.load(f))
-    avg_acc_th = OrderedDict()
-    for k in sorted([float(k.replace("acc_", "")) for k in results[norm][type].keys()]):
-        avg_acc_th[k] = 0
-    for k, v in results[norm][type].items():
-        avg_acc_at = np.array(v)
-        avg_acc_th[float(k.replace("acc_", ""))] = avg_acc_at
-    avg_acc = []
-    for k, v in avg_acc_th.items():
-        avg_acc.append(v)
-    avg_acc = np.array(avg_acc).mean(0)
-    for i, acc in enumerate(avg_acc):
-        name = load_dir.name.replace("acc", "avg_acc")
-        name = "_".join(name.split("_")[:9])
-        name = name.replace("_cw", "").replace("_ce", "") + f"_{i}"
-        dir = load_dir.parent / name
-        dir.mkdir()
-        np.save(str(dir / "avg_acc.npy"), acc)
+        p = "_".join(f.parent.name.split("_")[:6]).replace("acc", "avg_acc")
+        if "norand" in str(load_dir):
+            p = p + "_norand"
+        results[norm][type][f.stem][p].append(np.load(f))
+    avg_acc_th = DeepDict(OrderedDict)
+    for n in sorted(results[norm][type].keys(), key=lambda x: float(x.replace("acc_", ""))):
+        n_ = float(n.replace("acc_", ""))
+        for k in results[norm][type][n].keys():
+            k = k.replace("_cw", "").replace("_ce", "")
+            avg_acc_th[k][n] = 0
+    for n in results[norm][type]:
+        x = DeepDict(defaultdict)
+        for k, v in results[norm][type][n].items():
+            k = k.replace("_cw", "").replace("_ce", "")
+            x[k][n] = 1000
+        for k, v in results[norm][type][n].items():
+            avg_acc_at = np.array(v)
+            avg_acc_at = avg_acc_at.cumprod(1)
+            m = np.mean(avg_acc_at, (0, 2))[-1]
+            k = k.replace("_cw", "").replace("_ce", "")
+            if m < x[k][n]:
+                x[k][n] = m
+                avg_acc_th[k][n] = avg_acc_at
+    avg_acc = DeepDict(list)
+    for n, v in avg_acc_th.items():
+        for k, v in v.items():
+            avg_acc[n].append(v)
+        try:
+            assert np.array(avg_acc[n]).shape[0] == 5
+            avg_acc[n] = np.array(avg_acc[n]).mean(0)
+        except:
+            print(n)
+    for n in avg_acc.keys():
+        for i, acc in enumerate(avg_acc[n]):
+            name = n + f"_{i}"
+            dir = load_dir.parent / name
+            dir.mkdir()
+            np.save(str(dir / "avg_acc.npy"), acc)
